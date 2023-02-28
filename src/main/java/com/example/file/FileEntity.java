b983778f2d8d174a800e4512bdd8b5f2f3bee4a8
Ismoil <ismoilzaripov55@gmail.com>
2023-02-28 23:14:23 +0500

    File entity updated

--- src/main/java/com/example/file/FileEntity.java
+++ src/main/java/com/example/file/FileEntity.java
@@ -1,4 +1,26 @@
 package com.example.file;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="files")
 public class FileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String type;
+    private String path;
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
 }
