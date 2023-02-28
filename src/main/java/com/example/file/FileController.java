6a5b5267f0cc4804481f7aea512caf94b4b15aee
Ismoil <ismoilzaripov55@gmail.com>
2023-02-28 23:42:53 +0500

    uploadImageToSystem method successfully added !

--- src/main/java/com/example/file/FileController.java
+++ src/main/java/com/example/file/FileController.java
@@ -1,4 +1,23 @@
 package com.example.file;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/file")
 public class FileController {
+    private final FileService fileService;
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImageToSystem(
+            @RequestParam("image") MultipartFile request
+    ){
+        var response = fileService.saveToSystem(request);
+        return ResponseEntity.ok(response);
+    }
 }
