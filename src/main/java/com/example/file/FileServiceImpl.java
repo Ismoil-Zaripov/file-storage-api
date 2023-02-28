09cb367a6ff388ca774dc4c44750173204dd334f
Ismoil <ismoilzaripov55@gmail.com>
2023-02-28 23:36:12 +0500

    The saveToSystem method in the FileServiceImpl class has been updated

--- src/main/java/com/example/file/FileServiceImpl.java
+++ src/main/java/com/example/file/FileServiceImpl.java
@@ -1,14 +1,39 @@
 package com.example.file;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
+    private final FileRepository fileRepository;
+    @Value("${files.folder.path}")
+    private String filesFolderPath;
     @Override
-    public String saveToSystem(MultipartFile file) {
+    public String saveToSystem(MultipartFile fileRequest) {
+        String filePath = filesFolderPath + fileRequest.getOriginalFilename();
+        var file = FileEntity.builder()
+                .name(fileRequest.getOriginalFilename())
+                .type(fileRequest.getContentType())
+                .path(filePath)
+                .createdDate(LocalDateTime.now())
+                .build();
+
+        fileRepository.save(file);
+
+        try {
+            fileRequest.transferTo(new File(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (file != null) return "File successfully uploaded : " + filePath;
         return null;
     }
 
