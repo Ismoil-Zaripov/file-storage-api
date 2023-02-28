4f2de361b884b06800c1ec17f0228483971fcc3c
Ismoil <ismoilzaripov55@gmail.com>
2023-02-28 23:44:53 +0500

    downloadImageFromSystem method successfully added !

--- src/main/java/com/example/file/FileController.java
+++ src/main/java/com/example/file/FileController.java
@@ -1,11 +1,10 @@
 package com.example.file;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -19,5 +18,15 @@ public class FileController {
     ){
         var response = fileService.saveToSystem(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<?> downloadImageFromSystem(
+            @RequestParam String fileName
+    ){
+        var response = fileService.downloadImageFromSystem(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(response);
     }
 }
