6b0522bebc9588e91f2ee8774d35506f084e56ff
Ismoil <ismoilzaripov55@gmail.com>
2023-02-28 23:25:44 +0500

    Added downloadImageFromSystem method to FileService interface

--- src/main/java/com/example/file/FileService.java
+++ src/main/java/com/example/file/FileService.java
@@ -4,4 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
     String saveToSystem(MultipartFile file);
+    byte[] downloadImageFromSystem(String fileName);
 }
